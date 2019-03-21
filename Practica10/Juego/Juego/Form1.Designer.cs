57b85f3cb0eb7fc471e37aff5b5248d1b163d4ce
Josué Velázquez Gen <jvelazquezg1501@alumno.ipn.mx>
2019-03-21 10:50:01 -0600

    Add files via upload

---
+++ Practica10/Juego/Juego/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Juego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNovato = new System.Windows.Forms.Button();
+            this.btnGamer = new System.Windows.Forms.Button();
+            this.btnExperto = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(696, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tiempo: 60";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(21, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puntuación: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 750;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(266, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 56);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DUCK HUNT";
+            // 
+            // btnNovato
+            // 
+            this.btnNovato.BackColor = System.Drawing.Color.Transparent;
+            this.btnNovato.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNovato.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNovato.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovato.Location = new System.Drawing.Point(333, 126);
+            this.btnNovato.Name = "btnNovato";
+            this.btnNovato.Size = new System.Drawing.Size(118, 38);
+            this.btnNovato.TabIndex = 4;
+            this.btnNovato.Text = "Novato";
+            this.btnNovato.UseVisualStyleBackColor = false;
+            this.btnNovato.Click += new System.EventHandler(this.btnNovato_Click);
+            // 
+            // btnGamer
+            // 
+            this.btnGamer.BackColor = System.Drawing.Color.Transparent;
+            this.btnGamer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGamer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGamer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGamer.Location = new System.Drawing.Point(333, 171);
+            this.btnGamer.Name = "btnGamer";
+            this.btnGamer.Size = new System.Drawing.Size(118, 39);
+            this.btnGamer.TabIndex = 5;
+            this.btnGamer.Text = "Gamer";
+            this.btnGamer.UseVisualStyleBackColor = false;
+            this.btnGamer.Click += new System.EventHandler(this.btnGamer_Click);
+            // 
+            // btnExperto
+            // 
+            this.btnExperto.BackColor = System.Drawing.Color.Transparent;
+            this.btnExperto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExperto.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExperto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExperto.Location = new System.Drawing.Point(333, 217);
+            this.btnExperto.Name = "btnExperto";
+            this.btnExperto.Size = new System.Drawing.Size(118, 43);
+            this.btnExperto.TabIndex = 6;
+            this.btnExperto.Text = "Experto";
+            this.btnExperto.UseVisualStyleBackColor = false;
+            this.btnExperto.Click += new System.EventHandler(this.btnExperto_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Location = new System.Drawing.Point(351, 341);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Image = global::Juego.Properties.Resources.pato_i;
+            this.pictureBox1.Location = new System.Drawing.Point(638, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox2.Image = global::Juego.Properties.Resources.pato;
+            this.pictureBox2.Location = new System.Drawing.Point(638, 217);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(88, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox3.Image = global::Juego.Properties.Resources.pato;
+            this.pictureBox3.Location = new System.Drawing.Point(638, 67);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(88, 51);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.BackColor = System.Drawing.Color.Transparent;
+            this.btnPausa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPausa.Location = new System.Drawing.Point(749, 13);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(26, 30);
+            this.btnPausa.TabIndex = 10;
+            this.btnPausa.Text = "| |";
+            this.btnPausa.UseVisualStyleBackColor = false;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnContinuar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnContinuar.Location = new System.Drawing.Point(333, 108);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(118, 40);
+            this.btnContinuar.TabIndex = 11;
+            this.btnContinuar.Text = "CONTINUAR";
+            this.btnContinuar.UseVisualStyleBackColor = false;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMenu.Location = new System.Drawing.Point(333, 171);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(118, 40);
+            this.btnMenu.TabIndex = 12;
+            this.btnMenu.Text = "MENÚ PRINCIPAL";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Juego.Properties.Resources.sky;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(795, 397);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnContinuar);
+            this.Controls.Add(this.btnPausa);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnExperto);
+            this.Controls.Add(this.btnGamer);
+            this.Controls.Add(this.btnNovato);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Juego 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNovato;
+        private System.Windows.Forms.Button btnGamer;
+        private System.Windows.Forms.Button btnExperto;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.Button btnMenu;
+    }
+}
+
---
+++ Practica10/Juego/Juego/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Juego.Properties;
+
+namespace Juego
+{
+    public partial class Form1 : Form
+    {
+        int tiempo = 60;
+        int puntos = 0;
+        int dificultad = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Menue()
+        {
+            timer1.Stop();
+            timer2.Stop();
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = true;
+            btnPausa.Visible = false;
+            btnContinuar.Visible = false;
+            btnMenu.Visible = false;
+            btnExperto.Visible = true;
+            btnGamer.Visible = true;
+            btnNovato.Visible = true;
+            btnSalir.Visible = true;
+
+        }
+        private void Continuar()
+        {
+            if (dificultad == 1)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+            }
+            else if (dificultad == 2)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = false;
+            }
+            else if (dificultad == 3)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = true;
+            }
+            ObjetoVolador();
+
+        }
+        private void ObjetoVolador()
+        {
+            Random position = new Random();
+            if (dificultad == 1)
+            {
+                pictureBox1.Location = new Point(position.Next(915), position.Next(530));
+            }
+            else if (dificultad == 2)
+            {
+                pictureBox1.Location = new Point(position.Next(915), position.Next(530));
+                pictureBox2.Location = new Point(position.Next(915), position.Next(530));
+            }
+            else if (dificultad == 3)
+            {
+                pictureBox1.Location = new Point(position.Next(915), position.Next(530));
+                pictureBox2.Location = new Point(position.Next(915), position.Next(530));
+                pictureBox3.Location = new Point(position.Next(915), position.Next(530));
+            }
+        }
+
+        private void Empezar(int type)
+        {
+            btnPausa.Visible = true;
+            timer1.Start();
+            timer2.Start();
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            btnExperto.Visible = false;
+            btnGamer.Visible = false;
+            btnNovato.Visible = false;
+            btnSalir.Visible = false;
+            if (type == 1)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+                timer2.Interval = 1250;
+            }
+            else if (type == 2)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = false;
+                timer2.Interval = 950;
+            }
+            else if (type == 3)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = true;
+                timer2.Interval = 650;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tiempo == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                MessageBox.Show("Tu puntuación es: " + puntos, "TIEMPO TERMINADO",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+                puntos = 0;
+                tiempo = 60;
+
+                label1.Text = "Tiempo: " + tiempo;
+                label2.Text = "Puntuación: " + puntos;
+
+                timer1.Start();
+                timer2.Start();
+
+            }
+            else
+            {
+                tiempo = tiempo - 1;
+                label1.Text = "Tiempo: " + tiempo;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.pato;
+            pictureBox2.Image = Resources.pato;
+            pictureBox3.Image = Resources.pato;
+            ObjetoVolador();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Menue();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            this.Close();
+        }
+
+        private void btnNovato_Click(object sender, EventArgs e)
+        {
+            dificultad = 1;
+            Empezar(1);
+        }
+
+        private void btnGamer_Click(object sender, EventArgs e)
+        {
+            dificultad = 2;
+            Empezar(2);
+        }
+
+        private void btnExperto_Click(object sender, EventArgs e)
+        {
+            dificultad = 3;
+            Empezar(3);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            puntos = puntos + 1;
+            label2.Text = "Puntos: " + puntos;
+            pictureBox3.Image = Resources.pato2;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            puntos = puntos + 1;
+            label2.Text = "Puntos: " + puntos;
+            pictureBox2.Image = Resources.pato2;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            puntos = puntos + 1;
+            label2.Text = "Puntos: " + puntos;
+            pictureBox1.Image = Resources.pato2;
+        }
+        private void btnPausa_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            label3.Visible = true;
+            btnMenu.Visible = true;
+            btnContinuar.Visible = true;
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            MessageBox.Show("Tu puntuación es: " + puntos, "TIEMPO TERMINADO", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            puntos = 0;
+            tiempo = 60;
+            Menue();
+           
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            btnContinuar.Visible = false;
+            btnMenu.Visible = false;
+            timer1.Start();
+            timer2.Start();
+            label1.Visible = true;
+            label2.Visible = true;
+            Continuar();
+        }
+    }
+}
